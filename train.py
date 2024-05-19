6a5a6e0733d478fb10909aed9edc87675da20afe
gurnoor121198 <62958740+gurnoor121198@users.noreply.github.com>
2024-05-20 00:44:54 +0530

    udacity project
    there are different files for automl and hyper parameter tuning

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
 
 def clean_data(data):
     # Dict for cleaning data
@@ -16,10 +17,10 @@ def clean_data(data):
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
 
     # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
+    x_df = data.to_pandas_dataframe().dropna() # this will remove NA values
+    jobs = pd.get_dummies(x_df.job, prefix="job") # this will convert data into tabular form
+    x_df.drop("job", inplace=True, axis=1) # this will remove label as "job" 
+    x_df = x_df.join(jobs)   # that concatenates (joins) the elements of an iterable (like a list or tuple) into a single string . it will have lisr of all jobs 
     x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
     x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
     x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
@@ -54,14 +55,16 @@ def main():
     # TODO: Create TabularDataset using TabularDatasetFactory
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-    ds = ### YOUR CODE HERE ###
+    dataset_link="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    
+    ds = TabularDatasetFactory.from_delimited_files(path=dataset_link)
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###a
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=45)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
